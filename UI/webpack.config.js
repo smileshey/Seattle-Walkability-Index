57a37e5f15a5f653e5769db4f3406192b7898cf8
Ryan <smiles@hey.com>
2024-09-10 14:25:08 -0700

    resolving packaging issues

---
+++ UI/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  mode: 'development',
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    host: '0.0.0.0', // Allows access from outside the container
+    allowedHosts: 'all', // Allows any host
+    disableHostCheck: true // Disables host header check
+  }
+};
+
+
+
+
