bf4b70a1b1261954be718ebf53ef63d0e5e8dfe8
Ryan <smiles@hey.com>
2024-10-02 16:49:33 -0700

    fixed absolute addresses for deployment

--- UI/webpack.config.js
+++ UI/webpack.config.js
@@ -1,15 +1,27 @@
 const path = require('path');
 const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
 
+  const plugins = [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(argv.mode), // Define NODE_ENV for production builds
+    }),
+  ];
+
+  // Conditionally add BundleAnalyzerPlugin in development mode
+  if (!isProduction) {
+    plugins.push(new BundleAnalyzerPlugin());
+  }
+
   return {
-    entry: './src/main.tsx',
+    entry: './UI/src/main.tsx',
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
-      publicPath: '/',
+      publicPath: '/', // Serve files from the root in production
     },
     resolve: {
       extensions: ['.ts', '.tsx', '.js'],
@@ -30,10 +42,9 @@ module.exports = (env, argv) => {
     },
     mode: isProduction ? 'production' : 'development',
     optimization: {
-      // minimize: isProduction, // Minimize only in production
-      minimize: false,
+      minimize: isProduction, // Enable minimization only for production builds
     },
-    devtool: isProduction ? false : 'inline-source-map', // Disable source maps in production
+    devtool: isProduction ? false : 'inline-source-map', // Disable source maps in production for performance
     devServer: {
       static: {
         directory: path.join(__dirname, 'dist'),
@@ -41,15 +52,13 @@ module.exports = (env, argv) => {
       historyApiFallback: true, // For SPAs, enables serving index.html for 404s
       port: 8080, // Local server port
       hot: true, // Enable Hot Module Replacement
-      open: true, // Auto-open in the browser
+      open: true, // Auto-open in the browser during development
     },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(argv.mode), // Define NODE_ENV for production builds
-      }),
-    ],
+    plugins, // Use the updated plugins array
   };
 };
+
+
 
 
 
