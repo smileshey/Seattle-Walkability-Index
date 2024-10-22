cecb9e92c483c43c07e87bd70caaddfae94aefd7
Ryan <smiles@hey.com>
2024-10-22 16:38:09 -0700

    updated token; added crime data

--- UI/src/main.tsx
+++ UI/src/main.tsx
@@ -20,7 +20,7 @@ import { neighborhoodPopupTemplate, fishnetPopupTemplate } from './popup_templat
 
 const webMap = new WebMap({
   portalItem: {
-    id: '0c19f5906de2494aa862f534c3903fc4'
+    id: 'b364b35386f644a3b7964ab11963abd3'
   } 
 },
 );
